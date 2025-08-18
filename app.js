aade2b4b7c1e7e02a901816b89304dc7b0620fe9
LevisFreitas1771 <levisfreitas177@gmail.com>
2025-08-18 11:34:35 -0300

    add Get method and endpoints

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function(req,res){
+    res.send("Welcome on my website")
+});
+
+app.get("/articles", function(req,res){
+    res.send(`A R T I C L E S <br> 1 - How to crate an Android Apps. <br> 2 - How to use 'Node'.js <br> 3 - How to use Express`)
+});
+
+app.get("/contact", function(req,res){
+    res.send(`C O N T A C T S <br> 1 - INSTRAGRAM <br> 2 - WHATSAPP <br> 3 - FACEBOOK`)
+});
+
 app.listen(8081, function(){
     console.log("Server is running")
 });
